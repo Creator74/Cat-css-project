e707f36638fae04ec021e60560a337398f898a25
Creator74 <dhvanigupta2007@gmail.com>
2024-06-27 17:52:09 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useEffect, useRef } from 'react';
-import meowSound from "./assets/meow-Sound.mp3"
+import { useEffect, useRef } from "react";
+import meowSound from "./assets/meow-Sound.mp3";
 import headerImage from "./assets/header-cat-image.jpg";
+import nyanCatImage from "./assets/nyan-cat-image.jpg";
+import nyanCatSound from "./assets/nyan-cat-sound.mp3";
+import persianCatImage from "./assets/persian-cat-image.jpg";
+import moniCatImage from "./assets/moni-cat-image.jpeg";
 
-function Header() {
+function Header({ setHeaderHeight }) {
   const headerRef = useRef(null);
 
   useEffect(() => {
-    const updateMarginTop = () => {
+    const updateHeaderHeight = () => {
       if (headerRef.current) {
         const headerHeight = headerRef.current.clientHeight;
-        document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+        console.log("Header height:", headerHeight);
+        setHeaderHeight(headerHeight);
       }
     };
 
-    updateMarginTop();
-    window.addEventListener('resize', updateMarginTop);
-    return () => window.removeEventListener('resize', updateMarginTop);
-  }, []);
+    updateHeaderHeight();
+    window.addEventListener("resize", updateHeaderHeight);
+    return () => window.removeEventListener("resize", updateHeaderHeight);
+  }, [setHeaderHeight]);
 
   return (
     <div className="header-container" ref={headerRef}>
@@ -37,34 +42,63 @@ function Header() {
 }
 
 function Chicklet() {
-  const catImages = [headerImage, headerImage, headerImage, headerImage, headerImage, headerImage];
-  
-  const playMeowSound = () => {
-    const audio = new Audio(meowSound);
+  const catImages = [
+    headerImage,
+    headerImage,
+    headerImage,
+    moniCatImage,
+    persianCatImage,
+    nyanCatImage,
+  ];
+
+  const catSounds = [
+    meowSound,
+    meowSound,
+    meowSound,
+    meowSound,
+    meowSound,
+    nyanCatSound,
+  ];
+
+  function playMeowSound (index) 
+  {
+    const audio = new Audio(catSounds[index]);
     audio.play();
   };
 
   return (
-    <div className="content">
-      {catImages.map((image, index) => (
-        <div className="box" key={index}>
-          <img src={image} alt={`Cat ${index + 1}`} />
-          <p>Cat: Breed</p>
-          <div className="button-container">
-            <button className="order-btn">Order Now</button>
-            <button className="speaker-btn" onClick={playMeowSound}>🔊</button>
+    <div className="content-container">
+      <div className="content">
+        {catImages.map((image, index) => (
+          <div className="box" key={index}>
+            <img src={image} alt={`Cat ${index + 1}`} />
+            <p>Cat: Breed</p>
+            <div className="button-container">
+              <button className="order-btn">Order Now</button>
+              <button className="speaker-btn" onClick={() => playMeowSound(index)}>
+                🔊
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
 
-
 export default function MyApp() {
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--header-height",
+      `${headerHeight}px`
+    );
+  }, [headerHeight]);
+
   return (
     <div>
-      <Header />
+      <Header setHeaderHeight={setHeaderHeight} />
       <div className="app">
         <Chicklet />
       </div>
